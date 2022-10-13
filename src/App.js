9aa458b005b8132befe92425694d5b92e794fb7f
Yeto2 <bota.tll2004@gmail.com>
2022-10-13 22:47:50 +0100

    last commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
+import React , {useState} from 'react';
+import ReactMarkdown from 'react-markdown';
 import './App.css';
 
 function App() {
+  const [text , setText] = useState();
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <textarea 
+        id="editor" 
+        value={text}
+        onChange={(e)=>setText(e.target.value)}
+      />
+      <div className='preview'>
+      <ReactMarkdown children={text} />
+      </div>
+
     </div>
   );
 }
